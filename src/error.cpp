c8d5129afbb18b5ac126f7bce5dafb2da1bc20d7
Mespyr <senukcw@gmail.com>
2021-12-28 21:33:11 -0500

    made error messages output to stderr

--- src/error.cpp
+++ src/error.cpp
@@ -2,42 +2,42 @@
 
 void print_token_error(Token token, std::string message)
 {
-    std::cout << ERROR_COLOR << "[" << token.loc.file_location << "] error on line " << token.loc.line_number << " column " << token.loc.column_number_start << ": " << message << std::endl;
-    std::cout << token.loc.line << std::endl;
+    std::cerr << ERROR_COLOR << "[" << token.loc.file_location << "] error on line " << token.loc.line_number << " column " << token.loc.column_number_start << ": " << message << std::endl;
+    std::cerr << token.loc.line << std::endl;
 
     for (int i = 0; i < token.loc.column_number_start; i++)
-        std::cout << " ";
+        std::cerr << " ";
 
     for (int i = 0; i < token.loc.column_number_end - token.loc.column_number_start; i++)
-        std::cout << "^";
+        std::cerr << "^";
 
-    std::cout << RESET_COLOR << std::endl; 
+    std::cerr << RESET_COLOR << std::endl; 
 }
 
 void print_op_error(Op op, std::string message)
 {
-    std::cout << ERROR_COLOR << "[" << op.loc.file_location << "] error on line " << op.loc.line_number << " column " << op.loc.column_number_start << ": " << message << std::endl;
-    std::cout << op.loc.line << std::endl;
+    std::cerr << ERROR_COLOR << "[" << op.loc.file_location << "] error on line " << op.loc.line_number << " column " << op.loc.column_number_start << ": " << message << std::endl;
+    std::cerr << op.loc.line << std::endl;
 
     for (int i = 0; i < op.loc.column_number_start; i++)
-        std::cout << " ";
+        std::cerr << " ";
 
     for (int i = 0; i < op.loc.column_number_end - op.loc.column_number_start; i++)
-        std::cout << "^";
+        std::cerr << "^";
 
-    std::cout << RESET_COLOR << std::endl;
+    std::cerr << RESET_COLOR << std::endl;
 }
 
 void print_lexing_error(Location loc, std::string message)
 {
-    std::cout << ERROR_COLOR << "[" << loc.file_location << "] error on line " << loc.line_number << " column " << loc.column_number_start << ": " << message << std::endl;
-    std::cout << loc.line << std::endl;
+    std::cerr << ERROR_COLOR << "[" << loc.file_location << "] error on line " << loc.line_number << " column " << loc.column_number_start << ": " << message << std::endl;
+    std::cerr << loc.line << std::endl;
 
     for (int i = 0; i < loc.column_number_start; i++)
-        std::cout << " ";
+        std::cerr << " ";
 
     for (int i = 0; i < loc.column_number_end - loc.column_number_start; i++)
-        std::cout << "^";
+        std::cerr << "^";
 
-    std::cout << RESET_COLOR << std::endl;
+    std::cerr << RESET_COLOR << std::endl;
 }
