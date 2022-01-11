aea29beef9d4608f921f533d1ddc6c247450b604
Mespyr <senukcw@gmail.com>
2022-01-10 22:31:22 -0500

    added options to compiler to compile to nasm or fasm with flags, and added help flag

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,77 @@
 
 void usage(char* exec_name)
 {
-    std::cout << "USAGE: " << exec_name << " <filename>" << std::endl;
+    std::cout << "USAGE: " << exec_name << " <FILENAME> <OPTIONS>" << std::endl;
+    std::cout << "OPTIONS:" << std::endl;
+    std::cout << "\t-h, --help\tshow help"                    << std::endl;
+    std::cout << "\t-f, --fasm\tuse fasm to compile programs" << std::endl;
+    std::cout << "\t-n, --nasm\tuse nasm to compile programs" << std::endl;
+}
+
+struct Options
+{
+    ASSEMBLER assembler;
+    std::string filename;
+    bool help;
+};
+Options parse_argv(std::vector<std::string> argv)
+{
+    Options options;
+    options.assembler = NASM;
+    options.filename = "";
+    options.help = false;
+    for (long unsigned int i = 1; i < argv.size(); i++)
+    {
+        std::string arg = argv.at(i);
+        if (arg == "-f" || arg == "--fasm")
+            options.assembler = FASM;
+        else if (arg == "-n" || arg == "--nasm")
+            options.assembler = NASM;
+        else if (arg == "-h" || arg == "--help")
+            options.help = true;
+        else if (options.filename == "")
+            options.filename = arg;
+        else
+        {
+            print_error_with_no_location("more than one file provided for compilation.");
+            exit(1);
+        }
+    }
+
+    return options;
 }
 
 int main(int argc, char* argv[]) 
 {
-    if (argc == 1)
+    // parse argv
+    Options options = parse_argv(std::vector<std::string>(argv, argv + argc));
+
+    if (options.help)
+    {
+        usage(argv[0]);
+        exit(0);
+    }
+
+    // no filename passed into argv
+    if (options.filename == "")
     {
         usage(argv[0]);
         print_error_with_no_location("no file program for compilation.");
         exit(1);
     }
 
-    std::vector<Token> tokens = tokenize_file(argv[1]);
+    // parse file
+    std::vector<Token> tokens = tokenize_file(options.filename);
     std::map<std::string, Function> program = parse_tokens(tokens);
 
-    ASSEMBLER assembler = FASM;
-
     // compile
-    std::cout << INFO_COLOR << "[info] generating asm from '" << argv[1] << "'." << RESET_COLOR << std::endl;
-    compile_to_asm(program, "out.asm", assembler);
+    std::cout << INFO_COLOR << "[info] generating asm from '" << options.filename << "'." << RESET_COLOR << std::endl;
+    compile_to_asm(program, "out.asm", options.assembler);
 
     // nasm or fasm
     std::cout << INFO_COLOR << "[info] compiling assembly." << RESET_COLOR << std::endl;
 
-    if (assembler == FASM)
+    if (options.assembler == FASM)
     {
         exit_on_error(std::system("fasm out.asm"));
     }
